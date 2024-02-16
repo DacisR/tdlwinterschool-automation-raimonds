6f72e5599a9b97f6d042afe41b351c0d7aa3be42
Raimonds Dacis <dacisraimonds@gmail.com>
2024-02-16 18:35:57 +0200

    Added scenarios and steps discussed during lecture

--- features/login.feature
+++ features/login.feature
@@ -1,12 +1,32 @@
-Feature: The Internet Guinea Pig Website
+# Feature: The Internet Guinea Pig Website
 
-  Scenario Outline: As a user, I can log into the secure area
+#   Scenario Outline: As a user, I can log into the secure area
 
-    Given I am on the login page
-    When I login with <username> and <password>
-    Then I should see a flash message saying <message>
+#     Given I am on the login page
+#     When I login with <username> and <password>
+#     Then I should see a flash message saying <message>
 
-    Examples:
-      | username | password             | message                        |
-      | tomsmith | SuperSecretPassword! | You logged into a secure area! |
-      | foobar   | barfoo               | Your username is invalid!      |
+#     Examples:
+#       | username | password             | message                        |
+#       | tomsmith | SuperSecretPassword! | You logged into a secure area! |
+#       | foobar   | barfoo               | Your username is invalid!      |
+
+Feature: Login
+
+  Scenario: Successfully log in with valid username and password
+
+    Given I am on Login page
+    When I enter "tomsmith" username
+    And I enter "SuperSecretPassword!" password
+    And I press on Login button
+    Then I see a message "You logged into a secure area!"
+    And I see the Logout button
+
+  Scenario: Cannot log in with invalid username and password
+
+    Given I am on Login page
+    When I enter "foobar" username
+    And I enter "barfoo" password
+    And I press on Login button
+    Then I see a message "Your username is invalid!"
+    And I don't see the Logout button
--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,18 +1,66 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
 import { expect, $ } from '@wdio/globals'
 
-Given(/^I am on the (\w+) page$/, async (page) => {
-    await browser.url(`https://the-internet.herokuapp.com/${page}`);
+// Given(/^I am on the (\w+) page$/, async (page) => {
+//     await browser.url(`https://the-internet.herokuapp.com/${page}`);
+// });
+
+// When(/^I login with (\w+) and (.+)$/, async (username, password) => {
+//     await $('#username').setValue(username);
+//     await $('#password').setValue(password);
+//     await $('button[type="submit"]').click();
+// });
+
+// Then(/^I should see a flash message saying (.*)$/, async (message) => {
+//     await expect($('#flash')).toBeExisting();
+//     await expect($('#flash')).toHaveTextContaining(message);
+// });
+
+Given('I am on Login page', async function() {
+    await browser.navigateTo("https://the-internet.herokuapp.com/login");
 });
 
-When(/^I login with (\w+) and (.+)$/, async (username, password) => {
-    await $('#username').setValue(username);
-    await $('#password').setValue(password);
-    await $('button[type="submit"]').click();
+When('I enter "tomsmith" username', async function() {
+    await $('#username').setValue("tomsmith");
 });
 
-Then(/^I should see a flash message saying (.*)$/, async (message) => {
-    await expect($('#flash')).toBeExisting();
-    await expect($('#flash')).toHaveTextContaining(message);
+When('I enter "foobar" username', async function() {
+    await $('#username').setValue("foobar");
 });
 
+When('I enter "SuperSecretPassword!" password', async function() {
+    const passwordInput = await $("#password");
+    passwordInput.setValue("SuperSecretPassword!");
+});
+
+When('I enter "barfoo" password', async function() {
+    const passwordInput = await $("#password");
+    passwordInput.setValue("barfoo");
+});
+
+When('I press on Login button', async function() {
+    const button = await $("button[type=submit]");
+    button.click();
+});
+
+Then('I see a message "You logged into a secure area!"', async function() {
+    const flashMessage = await $("#flash");
+    // console.log(await flashMessage.getText());
+    await expect(flashMessage).toHaveText(expect.stringContaining("You logged into a secure area!"));
+});
+
+Then('I see a message "Your username is invalid!"', async function() {
+    const flashMessage = await $("#flash");
+    // console.log(await flashMessage.getText());
+    await expect(flashMessage).toHaveText(expect.stringContaining("Your username is invalid!"));
+});
+
+Then('I see the Logout button', async function() {
+    const logoutButton = await $("i*=Logout");
+    await expect(logoutButton).toBeDisplayed();
+});
+
+Then("I don't see the Logout button", async function() {
+    const logoutButton = await $("i*=Logout");
+    await expect(logoutButton).not.toBeDisplayed();
+});
